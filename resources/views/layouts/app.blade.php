74af96fd672f4740d59a33aa1d7a19cb3dd06413
Rotiwa Akinbo <oluwadurotiwa@gmail.com>
2022-03-27 16:33:59 +0100

    Updated but no buttons working

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -27,7 +27,7 @@
     @livewireStyles
 
     <!-- Scripts -->
-    <script src="{{ mix('js/app.js') }}" defer></script>
+    <script src="{{ asset('js/app.js') }}" defer></script>
 </head>
 
 <body>
